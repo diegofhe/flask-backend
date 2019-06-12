f9b43b6f8f9e7736850da08c3d584b3d2da38bb0
Diego Franco Personal <diegofhrg@gmail.com>
2019-06-12 10:03:40 -0500

    register test blueprint

--- src/main.py
+++ src/main.py
@@ -2,13 +2,14 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from src.models.app_user.routes import app_users
 from src.database import setDB
-from flask_migrate import Migrate
+from src.models.app_user.routes import app_users
+from src.models.test.routes import test
 
 
 app = Flask(__name__)
 setDB(app)
 app.register_blueprint(app_users)
+app.register_blueprint(test)
 
 
