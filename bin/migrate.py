9d144ea166127663a2bc37cd39e5f2bf17e845a4
Diego Franco Personal <diegofhrg@gmail.com>
2019-06-13 11:12:48 -0500

    Added: Migrate base on modular configuration

--- bin/migrate.py
+++ bin/migrate.py
@@ -1,17 +1,15 @@
-#https://flask-migrate.readthedocs.io/en/latest/
+# https://flask-migrate.readthedocs.io/en/latest/
 #
-#from base root directory
-#pipenv shell
-#python -m bin.migrate db init
-#python -m bin.migrate  db migrate
-#python -m bin.migrate  db upgrade
+# from base root directory
+# pipenv shell
+# python -m bin.migrate db init
+# python -m bin.migrate  db migrate
+# python -m bin.migrate  db upgrade
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from src.database import db
-
+from src.config.database import db
 
 app = Flask(__name__)
 
@@ -27,9 +25,8 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
-from src.models.app_user.definition import AppUser
 from src.models.test.definition import Test
-
+from src.models.user.definition import User
 
 if __name__ == '__main__':
     manager.run()
