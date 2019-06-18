f4fd43a98e749d11b79e1f8486fb08bdbcbf0e88
Diego Franco Personal <diegofhrg@gmail.com>
2019-06-17 21:27:47 -0500

    Modified: migration base on env variables

--- bin/migrate.py
+++ bin/migrate.py
@@ -1,27 +1,17 @@
 # https://flask-migrate.readthedocs.io/en/latest/
 #
 # from base root directory
-# pipenv shell
+# set all variables to connect to db
 # python -m bin.migrate db init
 # python -m bin.migrate  db migrate
 # python -m bin.migrate  db upgrade
 
-from flask import Flask
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from src.config.database import db
+from src.main import app
 
-app = Flask(__name__)
-
-db_url = 'localhost:3306'
-db_name = 'local'
-db_user = 'root'
-db_password = '@cuarroro00'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:@cuarroro00@localhost:3306/local'
-
-db.init_app(app)
 migrate = Migrate(app, db)
-
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
